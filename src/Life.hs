5e9dfa7682b39dc325908886a03a94aa08d83f40
Stephen Demos <stphndemos@gmail.com>
2017-03-26 17:28:32 -0400

    updated to new haskell stuff

--- src/Life.hs
+++ src/Life.hs
@@ -8,9 +8,6 @@ data Grid = Grid
     { unBoard  :: Array U DIM2 Int
     }
 
-tick :: Grid -> Grid
-tick = undefined
-
 play :: Grid -> Int -> Grid
 play = applyN tick
 
@@ -21,3 +18,6 @@ applyN = applyN' id
 applyN' :: (a -> a) -> (a -> a) -> a -> Int -> a
 applyN' k f a 0 = k a
 applyN' k f a i = applyN' (f . k) f a (i - 1)
+
+tick :: Grid -> Grid
+tick = undefined
